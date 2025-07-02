a2b6a98b31575542b98ec0a84ae3070eb4c47492
unknown <admin@skmin-test.Kanavi-Auto.com>
2025-07-02 15:54:38 +0900

    [CARE-102] feat: 페이지 모든 경로 설정

---
+++ src/pages/UserAccounScreen.tsx
@@ -0,0 +1,8 @@
+
+export default function UserAccountPage() {
+  return (
+    <div className="flex-1 bg-background flex items-center justify-center">
+      <h1 className="text-2xl font-bold text-gray-800">노인 정보 변경 페이지입니다.</h1>
+    </div>
+  );
+}
