81561b674d206b2c4ae2166d840d89bac348dcdb
jpark0506 <45231740+jpark0506@users.noreply.github.com>
2025-07-11 22:43:29 +0900

    feat : zod, react-form-hook 설치

---
+++ src/components/common/containers/CenteralizedVerticalContainer.tsx
@@ -0,0 +1,7 @@
+export default function CenteralizedVerticalContainer({
+    children,
+}: {
+    children: React.ReactNode;
+}) {
+    return <div className="flex flex-col items-center justify-center">{children}</div>;
+}
