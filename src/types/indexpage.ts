f195d0a8593158383b1a42d99e484951aedcad01
unknown <admin@skmin-test.Kanavi-Auto.com>
2025-07-21 09:33:45 +0900

    [CARE-103] feat: type 및 임시 더미데이터

---
+++ src/types/indexpage.ts
@@ -0,0 +1,80 @@
+export interface StatusListProps {
+    title: string;
+    sortText: string;
+    items: string[] | number[];
+    type: 'time' | 'count';
+    headerRight?: React.ReactNode;
+}
+
+
+import userImage1 from '@_assets/images/img_user_ex_01.png';
+import userImage2 from '@_assets/images/img_user_ex_02.png';
+//sos list 임시 데이터
+
+export const sosListData = [
+  {
+    name: '홍길동',
+    status: '진행중',
+    requestDate: '2025.01.23',
+    completeDate: '미완료',
+    image: userImage1,
+  },
+  {
+    name: '김영희',
+    status: '진행중',
+    requestDate: '2025.01.11',
+    completeDate: '미완료',
+    image: userImage2,
+  },
+  {
+    name: '이름',
+    status: '처리완료',
+    requestDate: '2025.01.03',
+    completeDate: '2025.01.20',
+    image: '',
+  },
+];
+
+//UserList 임시 데이터
+ export const UserListdata = [
+    {
+      name: '홍길동',
+      battery: '정상',
+      screen: '정상',
+      updated: '1분 전',
+      image: userImage1 ,
+      connected: true,
+    },
+    {
+      name: '김영희',
+      battery: '경고',
+      screen: '정상',
+      updated: '1분 전',
+      image: userImage2,
+      connected: true,
+    },
+    {
+      name: '이름',
+      battery: '정상',
+      screen: '경고',
+      updated: '12시간 전',
+      image: '',
+      connected: false,
+    },
+    {
+      name: '이름',
+      battery: '정상',
+      screen: '경고',
+      updated: '14시간 전',
+      image: '',
+      connected: false,
+    },
+    {
+      name: '이름',
+      battery: '경고',
+      screen: '경고',
+      updated: '16시간 전',
+      image: '',
+      connected: true,
+    },
+  ];
