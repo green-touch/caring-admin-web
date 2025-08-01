eda9d52ada4e9fa9b3bed959ecdaae3928da739b
unknown <admin@skmin-test.Kanavi-Auto.com>
2025-08-01 13:15:07 +0900

    [CARE-118]feat: SOS 내역 조회 페이지 구현

--- src/pages/SosHistoryPage.tsx
+++ src/pages/SosHistoryPage.tsx
@@ -1,8 +1,20 @@
+import SosList from "@_components/Indexpage/SosList";
+import SosHistoryFilterBox from "@_components/soshistorypage/HistoyFilter";
+
 
 export default function SosHistoryPage() {
   return (
-    <div className="flex-1 bg-background flex items-center justify-center">
-      <h1 className="text-2xl font-bold text-gray-800">SOS 내역 조회 페이지입니다.</h1>
+    <div className="px-[20px] mt-16 sm:px-[30px] md:px-[40px] lg:px-[50px] py-4">
+      <h1 className="text-[24px] leading-[150%] font-bold text-[#1D1D1D] mb-6 pl-6">
+        SOS내역 조회
+      </h1>
+      <div className="mt-10 mb-16 px-6">
+        <SosHistoryFilterBox />
+      </div>
+      <div className="mt-10 px-6">
+        <SosList title="요청 인원" count={3} showIcon={false} showSortBox={false} />
+      </div>
+
     </div>
   );
 }
